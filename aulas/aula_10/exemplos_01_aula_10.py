8d570f1168e9eccfa60fb9a1c8458b98cd40da7f
RomuloALabre <eng.romulolabre@gmail.com>
2023-02-07 20:43:08 -0300

    Aula 10

--- aulas/aula_10/exemplos_01_aula_10.py
+++ aulas/aula_10/exemplos_01_aula_10.py
@@ -1,5 +1,8 @@
 tempo = int(input('Quantos anos tem seu carro? '))
 if tempo<=3:
     print('Seu carro é novo')
-else
+else:
     print('Seu carro é usado')
+print('--FIM--')
+#ou
+print('Carro novo'if tempo<=3 else'carro velho')
