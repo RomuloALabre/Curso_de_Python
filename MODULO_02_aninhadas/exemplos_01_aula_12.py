85efa8ac5ca014e11fe6c42c45dbe17524212fa5
RomuloALabre <eng.romulolabre@gmail.com>
2023-02-19 13:25:16 -0300

    Aula_12

---
+++ MODULO_02_aninhadas/exemplos_01_aula_12.py
@@ -0,0 +1,10 @@
+nome = input('Digite um nome: ')
+if nome == 'Ana':
+    print(f'{nome}, você é a filha do Rômulo!')
+elif nome == 'Paula':
+    print(f'{nome}, você é esposa do Rômulo!')
+elif nome == 'Tina':
+    print(f'{nome}, mãe do Rômulo!')
+else:
+    print(f'{nome}, você deve ser a avó do Rômulo!')
+print(f'Tenha um ótimo dia, {nome}!')
