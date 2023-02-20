39790b3bce8912df9c5eb0fd8af857a613d46a35
RomuloALabre <eng.romulolabre@gmail.com>
2023-02-20 10:37:05 -0300

    exercicios aula 12

--- MODULO_02_aninhadas/aula_12_if_elif_else/42_exercicio_tipos_de_triangulos_aula_12.py
+++ MODULO_02_aninhadas/aula_12_if_elif_else/42_exercicio_tipos_de_triangulos_aula_12.py
@@ -5,3 +5,12 @@ que tipo de triângulo será formado:
 - isósceles: dois lados iguais
 - escaleno: todos os lados diferentes
 '''
+a = float(input('Tamanho Reta A: '))
+b = float(input('Tamanho Reta B: '))
+c = float(input('Tamanho Reta C: '))
+if a < b+c and b < a+c and c < a+b:
+    print('Forma um triângulo!')
+if a == b == c:
+    print('Forma um triângulo equilátero.')
+else:
+    print('NÃO forma Triângulo.')
--- MODULO_02_aninhadas/aula_12_if_elif_else/43_exercicio_calcule_imc_pessoa_aula_12.py
+++ MODULO_02_aninhadas/aula_12_if_elif_else/43_exercicio_calcule_imc_pessoa_aula_12.py
@@ -7,3 +7,19 @@ e mostre seu status, e acordo com a tabela abaixo:
 - 30 até 40: obesidade
 - acima de 40: obesidade mórbida
 '''
+peso = float(input('Digite o peso corporal: '))
+alt = float(input('Digite a altura: '))
+alt1 = alt * 1
+imc = peso / (alt1**2)
+
+print(f'Seu IMC é: {imc:.2f}')
+if imc < 18.5:
+    print('Abaixo do peso.')
+elif imc > 18.5 and imc <= 25:
+    print('Peso ideal!')
+elif imc > 25 and imc <= 30:
+    print('Sobrepeso!')
+elif imc > 30 and imc <= 40:
+    print('Obesidade.')
+else:
+    print('Obesidade mórbida.')
