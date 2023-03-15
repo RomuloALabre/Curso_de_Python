ab1cda744efae8dc769407f061f9b1cfca1c893a
RomuloALabre <eng.romulolabre@gmail.com>
2023-03-15 10:29:24 -0300

    exercicios AULA14 CONCLUIDOS

--- MODULO_02_aninhadas/aula_14_while/64_exercicio_tratando_valores_aula_14.py
+++ MODULO_02_aninhadas/aula_14_while/64_exercicio_tratando_valores_aula_14.py
@@ -2,3 +2,13 @@
 #   Crie um programa que leia vários números inteiros pelo teclado.
 #   O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 #   No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag "999").
+c = 0
+soma = 0
+while c >= 0:
+    num = int(input('Digite o número que deseja [999 para parar]: '))
+    if num == 999:
+        break
+    soma = soma + num
+    c += 1  
+print(f'Você digitou {c} números.')
+print(f'E a soma é: {soma}')
--- MODULO_02_aninhadas/aula_14_while/65_exercicio_tratando_valores_v2_aula_14.py
+++ MODULO_02_aninhadas/aula_14_while/65_exercicio_tratando_valores_v2_aula_14.py
@@ -2,3 +2,22 @@
 #   Crie um programa que leia vários números inteiros pelo teclado.
 #   No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
 #   O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+num = cont = soma = 0
+resp = 'S'
+maior = menor = 0
+while resp == 'S':
+    num = int(input('Digite um número que deseja calcular: '))
+    if cont == 0:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num <= menor:
+            menor = num
+    soma = soma + num
+    cont += 1
+    resp = str(input('Deseja continuar? (S/N): ')).strip().upper()[0]
+media = soma / cont
+print(f'A media é: {media}')
+print(f'O maior é: {maior}')
+print(f'O menor é: {menor}')
