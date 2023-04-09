1a06c1861dace3e6bec6a648e7574d2cb852388b
RomuloALabre <eng.romulolabre@gmail.com>
2023-04-09 15:45:00 -0300

    exercicios aula 17 concluidos

--- MODULO_03_LISTAS/aula17_listas/82_exercicio__aula_17.py
+++ MODULO_03_LISTAS/aula17_listas/82_exercicio__aula_17.py
@@ -4,3 +4,19 @@ Depois disso, crie duas listas extras que vão conter apenas os valores pares
 e os valores ímpares digitados, respectivamente.
 Ao final, mostre o conteúdo das três listas geradas.
 '''
+lista = []
+ímpar = []
+par = []
+while True:
+    lista.append(int(input('Digite um número para listar: ')))
+    cont = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+    if cont == 'N':
+        break
+for l in lista: #OU for i, l in enumerate(lista):
+    if l % 2 == 0:
+        par.append(l)
+    else:
+        ímpar.append(l)
+print(f'\nA lista digitada foi {lista}')
+print(f'Os números pares são: {par}')
+print(f'Os números ímpares são: {ímpar}\n')
--- MODULO_03_LISTAS/aula17_listas/83_exercicio__aula_17.py
+++ MODULO_03_LISTAS/aula17_listas/83_exercicio__aula_17.py
@@ -4,3 +4,18 @@ que use parênteses.
 Seu aplicativo deverá analisar se a expressão passada 
 está com os parênteses abertos e fechados na ordem correta.
 '''
+exp = str(input('Digite a expressão: '))
+lista = []
+for simb in exp:
+    if simb == '(':
+        lista.append('(')
+    elif simb == ')':
+        if len(lista) > 0:
+            lista.pop()        
+        else:
+            lista.append(')')
+            break
+if len(lista) > 0:
+    print('expressão inválida')
+else:
+    print('expressão OK')
