81a7fe94d068235e2149ee747989c5750e2150b3
RomuloALabre <eng.romulolabre@gmail.com>
2023-04-15 19:04:45 -0300

    exercicios 87 e 88

--- MODULO_03_LISTAS/aula18_listas_compostas_listas_dentro_de_listas/87_exercicio__aula_18.py
+++ MODULO_03_LISTAS/aula18_listas_compostas_listas_dentro_de_listas/87_exercicio__aula_18.py
@@ -4,3 +4,34 @@ a: a soma de todos os valores pares digitados
 b: a soma dos valores da terceira coluna
 c: o maior valor da segunda linha
 '''
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+pares = list()
+coluna3 = []
+maior = soma = somac = 0
+for l in range(0, 3):
+    for c in range(0, 3): 
+        matriz[l][c] = int(input(f'Digite valor da posição [{l}][{c}]: '))
+        #num = matriz[l][c]
+        if matriz[l][c] % 2 == 0:
+            pares.append(matriz[l][c])
+            soma = soma + matriz[l][c]
+        if c == 2:
+            coluna3.append(matriz[l][c])
+            somac = somac + matriz[l][c]
+        if l == 1:
+            if maior == 0:
+                maior = matriz[l][c]
+            elif maior <= matriz[l][c]:
+                maior = matriz[l][c]
+print('A matriz gerada foi: ')
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+print('-='*30)
+pares.sort()
+print(f'Os números pares que compõem essa MATRIZ são: {pares} cuja soma é: {soma}')
+print(f'Os valores da 3º coluna são: {coluna3} cuja soma é: {somac}')
+'''OU SEM CRIAR A LISTA COLUNA3'''
+print(f'OU buscando direto na matriz: {matriz[0][2]}, {matriz[1][2]}, {matriz[2][2]}')
+print(f'O maior número da 2º linha é: {maior}')
--- MODULO_03_LISTAS/aula18_listas_compostas_listas_dentro_de_listas/88_exercicio__aula_18.py
+++ MODULO_03_LISTAS/aula18_listas_compostas_listas_dentro_de_listas/88_exercicio__aula_18.py
@@ -4,3 +4,34 @@ O programa vai perguntar quantos jogos serão gerados
 e vai sortear 6 números entre 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta.
 '''
+from random import randint
+from time import sleep
+jogo = []
+listajogos = []
+total = 1
+
+print('-'*30)
+print('      JOGA DA MEGA SENA      ')
+print('-'*30)
+qtdjogos = int(input('Quantos jogos você quer fazer? '))
+while total <= qtdjogos:
+    cont = 0
+    while True:
+        sorteio = randint(1 , 60)
+        if sorteio not in jogo:
+            jogo.append(sorteio)
+            cont += 1
+        if cont >= 6:
+            break
+    total += 1
+    jogo.sort()
+    listajogos.append(jogo[:])
+    jogo.clear()
+print('-='*3, f'SORTEANDO {qtdjogos} JOGOS:', '-='*3)
+'''for c in range(0 , qtdjogos):
+    print(f'{listajogos[c]}')
+    sleep(0.5)'''
+for i, l in enumerate(listajogos):
+    sleep(1)
+    print(f'JOGO {i+1}: {listajogos[i]}')
+print('-='*5, '< BOA SORTE! >', '-='*5)
