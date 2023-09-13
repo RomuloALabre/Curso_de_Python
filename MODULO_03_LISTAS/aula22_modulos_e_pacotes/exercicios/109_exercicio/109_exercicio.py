0e68982dd2e12b64b5b101bfc152951e382d5670
RomuloALabre <eng.romulolabre@gmail.com>
2023-09-12 21:46:43 -0300

    exercicio 109

--- MODULO_03_LISTAS/aula22_modulos_e_pacotes/exercicios/109_exercicio/109_exercicio.py
+++ MODULO_03_LISTAS/aula22_modulos_e_pacotes/exercicios/109_exercicio/109_exercicio.py
@@ -3,3 +3,13 @@ Modifique as funções que foram criadas no desafio 107 para que elas aceitem um
 informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), 
 desenvolvido no desafio 108.
 """
+import moeda
+preço = float(input('Digite um valor: R$ '))
+taxa = float(input('Insira a taxa: '))
+print(f'O preço de {moeda.moeda(preço)} com uma taxa de {taxa:.0f} % fica um total de: {(moeda.aumentar(preço, taxa, True))}')
+
+print(f'O preço {moeda.moeda(preço)} com taxa de desconto de {taxa}% o valor fica: {(moeda.diminuir(preço, taxa, True))}')
+
+print(f'O dobro do preço é: {(moeda.dobro(preço, True))}')
+
+print(f'A metade do preço é: {(moeda.metade(preço, True))}')
